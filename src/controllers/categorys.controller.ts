4116a5718684948edd41e6433e6d5925811d57e5
Konstructa <elenprojects7@gmail.com>
2021-12-20 19:41:59 -0300

    impleementando funcionalidade

--- src/controllers/categorys.controller.ts
+++ src/controllers/categorys.controller.ts
@@ -3,13 +3,14 @@ import { connect } from '../sql/db';
 
 export async function getNamesCategorys(req: Request, res:Response): Promise<Response> {
   const conn = await connect();
-  const posts = await conn.query('SELECT classification FROM insights');
-  return res.status(302).send({ message: ['Aqui está a lista de categorias', posts[0]] });
+  const ver = await conn.query('SELECT classification FROM insights ORDER BY created_at DESC');
+  return res.status(302).send({ message: ['Aqui está a lista de categorias', ver[0]] });
 }
 
 export async function getElementsFromOneCategory(req: Request, res:Response): Promise<Response> {
-  const { category } = req.params;
+  const classification = req.params.category;
   const conn = await connect();
-  const posts = await conn.query(`SELECT * FROM insights WHERE classification = ${category}`);
+  const posts = await conn.query(`
+    SELECT * FROM insights WHERE classification = '${classification}' ORDER BY created_at DESC`);
   return res.status(302).send({ message: ['Aqui está os elementos da categoria que você está procurando', posts[0]] });
 }
