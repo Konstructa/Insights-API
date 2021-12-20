8d5df24106313edf014cb5d9f205888b4ed2bb3f
Konstructa <elenprojects7@gmail.com>
2021-12-20 19:42:25 -0300

    alterando o time

--- src/sql/db.sql
+++ src/sql/db.sql
@@ -1,17 +1,18 @@
+USE DATABASE insight_api;
+
 DROP DATABASE IF EXISTS insight_api;
 
 CREATE DATABASE IF NOT EXISTS insight_api;
 
 CREATE TABLE IF NOT EXISTS insights {
-    id INT(11) NOT NULL AUTO_COMPLETE PRIMARY KEY;
-    title VARCHAR(200) NOT NULL;
-    description_idea TEXT NOT NULL;
-    classification enum('Back-end', 'Design', 'Front-end', 'Mobile') NOT NULL;
-    ideia_url TEXT NOT NULL;
-    image_url TEXT;
-    created_at DATETIME NOT NULL;
+    id INT(11) NOT NULL AUTO_COMPLETE PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    description_idea TEXT NOT NULL,
+    classification enum('Back-end', 'Design', 'Front-end', 'Mobile') NOT NULL,
+    ideia_url TEXT NOT NULL,
+    image_url TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 };
-
 
 
 DESCRIBE insights;
