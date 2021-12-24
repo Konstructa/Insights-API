d6c1c6164f793cecb4dc92a9297b8943d5fd04e6
Konstructa <elenprojects7@gmail.com>
2021-12-23 21:55:07 -0300

    mudando erro e query

--- src/services/insights.services.ts
+++ src/services/insights.services.ts
@@ -24,7 +24,7 @@ export async function getInsightIdService(id: string) {
   /* if const posts = await conn.query('SELECT EXISTS(SELECT * from insights WHERE id = ?' [id]); */
   try {
     const conn = await connect();
-    const posts = await conn.query('SELECT * FROM insights WHERE id = ?', [id]);
+    const posts = await conn.query(`SELECT EXISTS (SELECT * FROM insights WHERE id ${id}) as truth`);
     return posts;
   } catch (e) {
     throw new Error('Insight não encontrado');
@@ -36,6 +36,6 @@ export async function deleteInsightIdService(id: string) {
     const conn = await connect();
     await conn.query('DELETE FROM insights WHERE id = ?', [id]);
   } catch (e) {
-    throw new Error('Você não tem permissão para deletar uma ideia/Não encontrada');
+    throw new Error('Não encontrada');
   }
 }
