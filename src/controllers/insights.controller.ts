f1cae8ae172a32cc53822e52847a56a7a504f909
Konstructa <elenprojects7@gmail.com>
2021-12-23 21:55:26 -0300

    adicionando verificação e mudando para json

--- src/controllers/insights.controller.ts
+++ src/controllers/insights.controller.ts
@@ -5,7 +5,7 @@ import * as use from '../services/insights.services';
 export async function getAllInsights(req: Request, res:Response): Promise<Response> {
   try {
     const [posts, totalInsights] = await use.getAllInsightsService();
-    return res.status(200).send({
+    return res.status(200).json({
       page: 1, results: posts[0], total_pages: 1, total_results: totalInsights[0],
     });
   } catch (e) {
@@ -17,13 +17,16 @@ export async function getAllInsights(req: Request, res:Response): Promise<Respon
 export async function createInsights(req: Request, res:Response) {
   const newInsight: Insight = req.body;
   try {
+    if (req.body.classification == null) {
+      throw new Error('Categoria não inserida');
+    }
     await use.createInsightsService(newInsight);
-    return res.status(201).send({
+    return res.status(201).json({
       message: 'Ideia enviada com sucesso!',
     });
   } catch (e) {
     const error = e as Error;
-    return res.status(400).send({
+    return res.status(400).json({
       message: error.message,
     });
   }
@@ -33,13 +36,13 @@ export async function getInsightById(req: Request, res:Response): Promise<Respon
   const id = req.params.id_ideia;
   try {
     const posts = await use.getInsightIdService(id);
-    return res.status(302).send({
+    return res.status(302).json({
       message: 'sucess', results: posts[0],
     });
   } catch (e) {
     const error = e as Error;
-    return res.status(404).send({
-      message: error.message,
+    return res.status(404).json({
+      message: 'false', results: error.message,
     });
   }
 }
@@ -48,12 +51,12 @@ export async function deleteInsightId(req: Request, res:Response) {
   const id = req.params.id_ideia;
   try {
     await use.deleteInsightIdService(id);
-    return res.status(202).send({
+    return res.status(202).json({
       message: 'Ideia deletada',
     });
   } catch (e) {
     const error = e as Error;
-    return res.status(405).send({
+    return res.status(405).json({
       message: error.message,
     });
   }
