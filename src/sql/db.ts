dea8d0acad61fb104675076c31d23133a6b88fef
Konstructa <elenprojects7@gmail.com>
2021-12-27 21:04:28 -0300

    alterando variaves de ambiente

--- src/sql/db.ts
+++ src/sql/db.ts
@@ -3,10 +3,10 @@ import { createPool } from 'mysql2/promise';
 
 export async function connect() {
   const connection = createPool({
-    host: process.env.HOST || 'localhost',
-    user: process.env.USER || 'root',
-    password: process.env.PASSWORD || '',
-    database: 'insight_api',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'insight_api',
     connectionLimit: 10,
   });
 
