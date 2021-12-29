d94a3af84945bb093b45450060b9f0a162e9038c
Konstructa <elenprojects7@gmail.com>
2021-12-29 00:10:18 -0300

    adicionando checagem se existe id

--- src/services/insight.services.ts
+++ src/services/insight.services.ts
@@ -1,11 +1,18 @@
 import { connect } from '../sql/db';
 
 export async function getInsightIdService(id: string) {
-  /* if const posts = await conn.query('SELECT EXISTS(SELECT * from insights WHERE id = ?' [id]); */
   try {
     const conn = await connect();
-    const posts = await conn.query(`SELECT EXISTS (SELECT * FROM insights WHERE id = ${id}) as truth`);
-    return posts;
+    const checkExists = await conn.query(`SELECT EXISTS (SELECT * FROM insights WHERE id = ${id}) as exist`);
+    const checkParse = JSON.parse(JSON.stringify(checkExists[0]));
+    const count = {
+      a: checkParse[0].exist,
+    };
+    if (count.a > 0) {
+      const posts = await conn.query('SELECT * from insights WHERE id = ?', [id]);
+      return posts;
+    }
+    throw new Error('Insight não encontrado');
   } catch (e) {
     throw new Error('Insight não encontrado');
   }
