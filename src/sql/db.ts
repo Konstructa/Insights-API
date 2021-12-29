b673e835abfc466b72d212bbcbd12a9383527614
Konstructa <elenprojects7@gmail.com>
2021-12-29 17:10:03 -0300

    apagando os dados

--- src/sql/db.ts
+++ src/sql/db.ts
@@ -3,10 +3,10 @@ import { createPool } from 'mysql2/promise';
 
 export async function connect() {
   const connection = createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'insight_api',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectionLimit: 10,
   });
 
