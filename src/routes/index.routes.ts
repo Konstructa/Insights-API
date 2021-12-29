59fa945ee42ecb6ba5046fe3cfbc61da53b73c8f
Konstructa <elenprojects7@gmail.com>
2021-12-29 17:15:24 -0300

    voltando a usar get

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,6 +3,7 @@ import { indexWelcome } from '../controllers/index.controller';
 
 const router = Router();
 
-router.use('/', indexWelcome);
+router.route('/')
+  .get(indexWelcome);
 
 export default router;
