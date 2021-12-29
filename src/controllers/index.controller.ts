4c398fa550a1fc847e495a6f29e774f9c3b95c65
Konstructa <elenprojects7@gmail.com>
2021-12-29 00:08:45 -0300

    adicionando arquivo html

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
 
 export function indexWelcome(req: Request, res: Response) {
-  return res.status(200).json({
-    menssage: ['Essa é uma API de sugestões de cursos das áreas de Front-End, Back-End, Mobile e Design'],
-  });
+  return res.status(200).sendFile(`${process.cwd()}/src/public/index.html`);
 }
