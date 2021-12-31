64166cbfb5e30c8c23b68f08abf8ecb53c7fe210
Konstructa <elenprojects7@gmail.com>
2021-12-30 21:43:49 -0300

    delete

--- dist/controllers/index.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexWelcome = void 0;
-function indexWelcome(req, res) {
-    return res.status(200).json({
-        menssage: ['Essa é uma API de sugestões de cursos das áreas de Front-End, Back-End, Mobile e Design'],
-    });
-}
-exports.indexWelcome = indexWelcome;
