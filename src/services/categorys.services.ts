a951ecc76a0635903b04eda4817dc188dc622b0b
Konstructa <elenprojects7@gmail.com>
2022-01-03 01:00:46 -0300

    consertando a quantidade

--- src/services/categorys.services.ts
+++ src/services/categorys.services.ts
@@ -3,7 +3,7 @@ import { connect } from '../sql/db';
 export async function getNamesCategorysService() {
   try {
     const conn = await connect();
-    const posts = await conn.query('SELECT classification FROM insights ORDER BY created_at DESC');
+    const posts = await conn.query('SELECT classification FROM insights GROUP BY classification ORDER BY created_at DESC');
     return posts;
   } catch (e) {
     throw new Error('Categoria não encontrada');
