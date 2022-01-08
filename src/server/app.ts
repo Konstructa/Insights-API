00269b1007839e700b98e30f094d7e9472a3e223
Konstructa <elenprojects7@gmail.com>
2022-01-08 18:50:43 -0300

    add delete

--- src/server/app.ts
+++ src/server/app.ts
@@ -36,16 +36,16 @@ export class App {
     this.app.use((req, res, next): void => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Headers', '*');
-      res.header('Access-Control-Allow-Methods', 'GET, POST');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
       next();
     });
   }
 
   routes() {
     this.app.use(IndexRoutes);
+    this.app.use('/api', InsightsRoutes);
     this.app.use('/api/vitrine', VitrineRoutes);
     this.app.use('/api/insight', InsightRoutes);
-    this.app.use('/api', InsightsRoutes);
     this.app.use('/api/category', CategoryRoutes);
     this.app.use(ErrorRoutes);
   }
